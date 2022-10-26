50b8e77ad97618500050e0e1ef543622231a7dbb
linda2927 <linda2927@naver.com>
2022-10-26 16:12:19 +0900

    Week 3 - Custom Renderer, Exceptions, Filter and Search

--- z9-api/config/middleware.py
+++
@@ -1,9 +0,0 @@
-class AddHeaders:
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response['Host'] = "localhost"
-        return response
